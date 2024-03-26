77eef7601bad19c727d2262f19b7fe4f3f46871f
Svyatoslav Krivosheev <t1ksy@mail.ru>
2024-03-26 18:46:06 +0300

    complete task_5.py

---
+++ seminar_1/task_5.py
@@ -0,0 +1,38 @@
+# Задание №5.
+# Написать функцию, которая будет выводить на экран HTML страницу с заголовком "Моя первая HTML страница"
+# и абзацем "Привет, мир!".
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+first = '''
+<h1> Моя первая страница</h1>
+<p>Привет мир</p>
+'''
+
+@app.route('/')
+def hello():
+    return first
+
+
+# @app.route('/about/')
+# def about():
+#     return 'Страница с описанием сайта'
+#
+#
+# @app.route('/contact/')
+# def contact():
+#     return 'Страница для обратной связи'
+#
+#
+# @app.route('/<int:a>/<int:b>/')
+# def summa(a, b):
+#     return str(a + b)
+#
+#
+# @app.route('/<string:text>/')
+# def str_length(text):
+#     return str(len(text))
+
+if __name__ == '__main__':
+    app.run(debug=True)
