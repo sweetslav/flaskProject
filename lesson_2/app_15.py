ac41c0af1da1424e35eedf5f5e326631a9304c77
Svyatoslav Krivosheev <t1ksy@mail.ru>
2024-03-27 21:55:14 +0300

    complete app_15.py and main.py

---
+++ lesson_2/app_15.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, make_response, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    context = {
+        'title': 'Главная',
+        'name': 'Святослав',
+    }
+    response = make_response(render_template('main.html', **context))
+    response.headers['new_head'] = 'New_value'
+    response.set_cookie('username', context['name'])
+    return response
+
+
+@app.route('/getcookie/')
+def get_cookies():
+    # Получаем значение cookie
+    name = request.cookies.get('username')
+    return f'Значение cookie: {name}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
