6415faf8c10946c0b21489e44b64276e9db154ce
Svyatoslav Krivosheev <t1ksy@mail.ru>
2024-03-29 14:07:22 +0300

    create task_1

---
+++ seminar_2/app_01.py
@@ -0,0 +1,27 @@
+# Задание №1.
+# Создать страницу, на которой будет кнопка "Нажми меня".
+# При нажатии на кнопку будет переход на другую страницу с приветствием пользователя по имени.
+
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def html_index():
+    return render_template('index.html')
+
+
+@app.get('/form/')
+def form_view_get():
+    return render_template('form.html')
+
+
+@app.post('/submit/')
+def submit():
+    name = request.form.get('name')
+    return f'Hello, {name}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
