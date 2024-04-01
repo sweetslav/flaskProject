12f3e7f8fb97689515774c4579a101200d233d32
Svyatoslav Krivosheev <t1ksy@mail.ru>
2024-04-01 22:04:47 +0300

    Create forms_2.py/ RegisterForm class

---
+++ lesson_3/forms_2.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.fields.numeric import IntegerField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+
+class RegisterForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
+    gender = StringField('Gender', choices=[('male', 'мужчина'), ('female', 'женщина')])
